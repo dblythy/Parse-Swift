d5ee27cbd99b1b414c6a47c6cfe0529bf272e9e2
Corey <coreyearleon@icloud.com>
2021-06-16 15:56:28 -0400

    Decode parse otherCode error and add tests

--- Tests/ParseSwiftTests/ParseErrorTests.swift
+++ Tests/ParseSwiftTests/ParseErrorTests.swift
@@ -28,19 +28,10 @@ class ParseErrorTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         MockURLProtocol.removeAll()
-        #if !os(Linux) && !os(Android) && !os(Windows)
+        #if !os(Linux) && !os(Android)
         try KeychainStore.shared.deleteAll()
         #endif
         try ParseStorage.shared.deleteAll()
-    }
-
-    func testInitializers() throws {
-        let error = ParseError(code: .accountAlreadyLinked, message: "hello")
-        let expected = "ParseError code=208 error=hello"
-        XCTAssertEqual(error.description, expected)
-        let error2 = ParseError(otherCode: 593, message: "yolo")
-        let expected2 = "error=yolo otherCode=593"
-        XCTAssertTrue(error2.description.contains(expected2))
     }
 
     func testEncode() throws {
@@ -53,71 +44,21 @@ class ParseErrorTests: XCTestCase {
         let decoded = try ParseCoding.jsonDecoder().decode(ParseError.self, from: encoded)
         XCTAssertEqual(decoded.code.rawValue, code)
         XCTAssertEqual(decoded.message, message)
-        XCTAssertNil(decoded.error)
-        XCTAssertEqual(decoded.debugDescription, "ParseError code=\(code) error=\(message)")
-        XCTAssertEqual(decoded.description, "ParseError code=\(code) error=\(message)")
-        XCTAssertEqual(decoded.errorDescription, "ParseError code=\(code) error=\(message)")
-    }
-
-    func testEncodeMessage() throws {
-        let code = -1
-        let message = "testing ParseError"
-        guard let encoded: Data = "{\"message\":\"\(message)\",\"code\":\(code)}".data(using: .utf8) else {
-            XCTFail("Should have unwrapped")
-            return
-        }
-        let decoded = try ParseCoding.jsonDecoder().decode(ParseError.self, from: encoded)
-        XCTAssertEqual(decoded.code.rawValue, code)
-        XCTAssertEqual(decoded.message, message)
-        XCTAssertNil(decoded.error)
-        XCTAssertEqual(decoded.debugDescription, "ParseError code=\(code) error=\(message)")
-        XCTAssertEqual(decoded.description, "ParseError code=\(code) error=\(message)")
-        XCTAssertEqual(decoded.errorDescription, "ParseError code=\(code) error=\(message)")
+        XCTAssertEqual(decoded.localizedDescription, "ParseError code=\(code) error=\(message)")
     }
 
     func testEncodeOther() throws {
         let code = 2000
         let message = "testing ParseError"
-        guard let encoded = "{\"error\":\"\(message)\",\"code\":\(code)}".data(using: .utf8) else {
+        guard let encoded: Data = "{\"error\":\"\(message)\",\"code\":\(code)}".data(using: .utf8) else {
             XCTFail("Should have unwrapped")
             return
         }
         let decoded = try ParseCoding.jsonDecoder().decode(ParseError.self, from: encoded)
         XCTAssertEqual(decoded.code, .other)
         XCTAssertEqual(decoded.message, message)
-        XCTAssertNil(decoded.error)
-        XCTAssertEqual(decoded.debugDescription,
+        XCTAssertEqual(decoded.localizedDescription,
                        "ParseError code=\(ParseError.Code.other.rawValue) error=\(message) otherCode=\(code)")
         XCTAssertEqual(decoded.otherCode, code)
-    }
-
-    func testCompare() throws {
-        let code = ParseError.Code.objectNotFound.rawValue
-        let message = "testing ParseError"
-        guard let encoded = "{\"error\":\"\(message)\",\"code\":\(code)}".data(using: .utf8) else {
-            XCTFail("Should have unwrapped")
-            return
-        }
-        let decoded = try ParseCoding.jsonDecoder().decode(ParseError.self, from: encoded)
-
-        let error: Error = decoded
-
-        XCTAssertTrue(error.equalsTo(.objectNotFound))
-        XCTAssertFalse(error.equalsTo(.invalidQuery))
-
-        XCTAssertTrue(error.containedIn(.objectNotFound, .invalidQuery))
-        XCTAssertFalse(error.containedIn(.operationForbidden, .invalidQuery))
-
-        XCTAssertTrue(error.containedIn([.objectNotFound, .invalidQuery]))
-        XCTAssertFalse(error.containedIn([.operationForbidden, .invalidQuery]))
-
-        XCTAssertNotNil(error.equalsTo(.objectNotFound))
-        XCTAssertNil(error.equalsTo(.invalidQuery))
-
-        XCTAssertNotNil(error.containedIn(.objectNotFound, .invalidQuery))
-        XCTAssertNil(error.containedIn(.operationForbidden, .invalidQuery))
-
-        XCTAssertNotNil(error.containedIn([.objectNotFound, .invalidQuery]))
-        XCTAssertNil(error.containedIn([.operationForbidden, .invalidQuery]))
     }
 }
